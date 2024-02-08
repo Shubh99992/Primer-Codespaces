d063c2fa8d3921ad44487549c9eae86adb450913
Shubham <105529358+Shubh99992@users.noreply.github.com>
2024-02-08 14:11:02 +0000

    profile adit

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Shubham Poddar",
+  title: "Web + Android Developer",
+  email: "sose.20210163215@rpvvsector5dwarka.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
